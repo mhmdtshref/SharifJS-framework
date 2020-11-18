ac1088685a9f8ea129985d7344a7fd66986cc334
Mohamed <mshareef@kiitos-tech.com>
2020-11-18 23:40:02 +0200

    Fix linter errors #8

--- src/constants/env.constants.ts
+++ src/constants/env.constants.ts
@@ -1,11 +1,10 @@
-
-const env: NodeJS.ProcessEnv = process.env;
+const env = process.env;
 
 export class EnvConstants {
-    database = {
-        url: env.DATABASE_URL || '',
-    }
+  database = {
+    url: env.DATABASE_URL || '',
+  };
 
-    host = env.HOST || '';
-    port = env.PORT || 5000;
+  host = env.HOST || '';
+  port = env.PORT || 5000;
 }
