897e3d63f2abb8b7d7dc43dcfa0443224487769d
Mohamed <mshareef@kiitos-tech.com>
2020-12-04 18:49:29 +0200

    Fix tsc errors

--- src/utils/encryption.util.ts
+++ src/utils/encryption.util.ts
@@ -1,10 +1,10 @@
 import bcrypt from 'bcrypt';
 import { EnvConstants } from '../constants';
 
-const { cryptoSaltRounds } = new EnvConstants();
+const { saltRounds } = EnvConstants.encryption;
 export class EncryptionUtil {
   generateHash = (plain: string) => {
-    return bcrypt.hash(plain, cryptoSaltRounds);
+    return bcrypt.hash(plain, saltRounds);
   };
 
   compareHash = (plain: string, hash: string) => {
